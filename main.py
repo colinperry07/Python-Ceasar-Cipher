6a1bf1d9b0739b63c6fff867fc32c859a4a1c5f9
Colin Perry <123716435+colinperry07@users.noreply.github.com>
2025-04-01 22:29:01 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+class Ceasar():
+    
+    
+    def __init__(self):
+        self.LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+    
+    
+    def encrypt(self, message, key):
+        encrypted_message = ''
+        self.message = message
+        self.key = key
+        
+        try:
+            for character in self.message:
+                character_pos = self.LETTERS.find(character)
+                encrypted_message += self.LETTERS[character_pos + self.key]
+        except:
+            pass
+        return encrypted_message
+
+
+    def decrypt(self, message, key):
+        self.message = message
+        self.key = key
+        decrypted_message = ''
+        try:
+            for character in self.message:
+                character_pos = self.LETTERS.find(character)
+                decrypted_message += self.LETTERS[character_pos - self.key]
+        except:
+            pass
+        return decrypted_message
+
+if __name__ == "__main__":
+    cipher = Ceasar()
+    
+    
+    try:
+        
+        init_message = str(input("Input the message you'd like to encrypt/decrypt: "))
+        key_choice = int(input("Enter the key number you would like to encrpyt/decrypt with (1-25): "))
+        crypt_choice = str(input("Would you like to encrypt or decrypt the message: "))
+        
+        if crypt_choice.lower() == "encrypt":
+            crypt_choice = True
+        if crypt_choice.lower() == "decrypt":
+            crypt_choice = False
+
+    except:
+        pass
+    
+    
+    if crypt_choice:
+        print(cipher.encrypt(init_message.lower(), key_choice))
+    else:
+        print(cipher.decrypt(init_message.lower(), key_choice))
