f868e4b59882aa282fc59653ebbb53ec2fd6b6ae
Colin Perry <123716435+colinperry07@users.noreply.github.com>
2025-04-01 23:40:45 -0400

    Update main.py
    change all key variables to be named 'offset'

--- main.py
+++ main.py
@@ -5,24 +5,24 @@ class Caesar():
         self.LETTERS = 'abcdefghijklmnopqrstuvwxyz'
     
     
-    def encrypt(self, message, key):
+    def encrypt(self, message, offset):
         encrypted_message = []
         for character in message:
             if character in self.LETTERS:
                 character_pos = self.LETTERS.find(character)
-                new_pos = (character_pos + key) % len(self.LETTERS)
+                new_pos = (character_pos + offset) % len(self.LETTERS)
                 encrypted_message.append(self.LETTERS[new_pos])
             else:
                 encrypted_message.append(character)
         return ''.join(encrypted_message)
 
 
-    def decrypt(self, message, key):
+    def decrypt(self, message, offset):
         decrypted_message = []
         for character in message:
             if character in self.LETTERS:
                 character_pos = self.LETTERS.find(character)
-                new_pos = (character_pos - key) % len(self.LETTERS)
+                new_pos = (character_pos - offset) % len(self.LETTERS)
                 decrypted_message.append(self.LETTERS[new_pos])
             else:
                 decrypted_message.append(character)
@@ -36,10 +36,10 @@ if __name__ == "__main__":
         
         init_message = str(input("Input the message you'd like to encrypt/decrypt: "))
         while True:
-            key_choice = int(input("Enter the key number you would like to encrpyt/decrypt with (1-25): "))
-            if 0 > key_choice > 25:
-                key_choice = None
-                print("invalid key number")
+            offset_choice = int(input("Enter the offset number you would like to encrpyt/decrypt with (1-25): "))
+            if 0 > offset_choice > 25:
+                offset_choice = None
+                print("invalid offset number")
             else:
                 break
         crypt_choice = str(input("Would you like to encrypt or decrypt the message: "))
@@ -54,6 +54,6 @@ if __name__ == "__main__":
     
     
     if crypt_choice:
-        print(cipher.encrypt(init_message.lower(), key_choice))
+        print(cipher.encrypt(init_message.lower(), offset_choice))
     else:
-        print(cipher.decrypt(init_message.lower(), key_choice))
+        print(cipher.decrypt(init_message.lower(), offset_choice))
